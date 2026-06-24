f099297ef772062e8ecdb73f17554bb6a405fc4a
Anushkasrivastava07 <anushkasrivastava020@gmail.com>
2026-06-24 17:26:37 +0530

    Add json

--- server/index.js
+++ server/index.js
@@ -3,9 +3,23 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("TeamUp Backend Running 🚀");
+  res.send("TeamUp Backend Running ");
 });
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
+});
+app.get("/teams", (req, res) => {
+  res.json([
+    {
+      teamName: "AI Innovators",
+      role: "Need 3 Developers",
+      members: 4
+    },
+    {
+      teamName: "Hack Masters",
+      role: "Need MERN Developer",
+      members: 5
+    }
+  ]);
 });
