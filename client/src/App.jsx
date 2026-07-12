207d3f8b7be8e2e55f5bb70b409369bb39ec8223
Anushkasrivastava07 <anushkasrivastava020@gmail.com>
2026-07-12 14:31:42 +0530

    feat: add routing for home and create team pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import SearchBar from "./components/SearchBar";
 import FeaturedTeams from "./components/FeaturedTeams";
 import Working from "./components/Working";
 import Footer from "./components/Footer";
+
+import CreateTeam from "./pages/CreateTeam";
 
 function App() {
   const [search, setSearch] = useState("");
