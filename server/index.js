4d62bfea16976aaa011f3bb81877f70682649069
Anushkasrivastava07 <anushkasrivastava020@gmail.com>
2026-08-11 18:51:25 +0530

    Implement JWT authentication

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const Team = require("./models/Team");
 const JoinRequest = require("./models/JoinRequest");
+const User = require("./models/User");
+const authMiddleware = require("./middleware/authMiddleware");
 
 const app = express();
 
@@ -85,6 +89,100 @@ app.post("/teams/:teamId/join", async (req, res) => {
 
     res.status(500).json({
       error: "Failed to send join request",
+    });
+  }
+});
+// POST Signup
+app.post("/auth/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        error: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User registered successfully!",
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      error: "Signup failed",
+    });
+  }
+});
+// POST Login
+app.post("/auth/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        error: "Invalid email or password",
+      });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({
+        error: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful!",
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      error: "Login failed",
+    });
+  }
+});
+// Protected Test Route
+app.get("/auth/me", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        error: "User not found",
+      });
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      error: "Failed to get user",
     });
   }
 });
